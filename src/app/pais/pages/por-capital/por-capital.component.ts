53c6236027f9079e1a07908680bbb6081435d420
DESKTOP-849DAC0\citti3 <barboza_luiscarlos96@outlook.com>
2022-04-28 17:56:13 -0500

    third commit

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent{
 
-  constructor() { }
+  paises: Country[] = [];
+  hayError: boolean = false;
+  termino:string = "";
 
-  ngOnInit(): void {
+  constructor(private paisService:PaisService) { }
+  
+  buscar(termino:any){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital(this.termino)
+      .subscribe((paises)=>{
+        this.paises = paises;
+      }, (err)=>{
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
+  sugerencias(termino:any){
+    this.hayError = false;
+  }
+
 
 }
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -10,20 +11,29 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = ""
+  hayError:boolean = false;
+  paises: Country[] = []
 
+  
   constructor(private paisService: PaisService) { 
-
-
-
+    
   }
 
-  buscar(){
-    console.log(this.termino);
+  buscar(termino:string){
+    this.hayError = false;
+    this.termino = termino;
     this.paisService.buscarPais(this.termino)
-      .subscribe(resp =>{
-        console.log(resp);
-        
+      .subscribe(paises =>{
+        console.log(paises);
+        this.paises = paises;
+      }, (err) =>{
+        this.hayError=true;
+        this.paises = [];
       })
+  }
+
+  sugerencias(termino:string){
+    this.hayError = false;
   }
 
 }
